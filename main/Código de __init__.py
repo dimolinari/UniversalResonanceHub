a2a0cbfd51003797414de43077b407c21e20389e
dimolinari <109002503+dimolinari@users.noreply.github.com>
2025-07-03 09:16:47 -0400

    Create Código de __init__.py

---
+++ main/Código de __init__.py
@@ -0,0 +1,6 @@
+# __init__.py
+# Módulo simbólico fractal iniciado por Dicaoz 🇪🇨
+
+from .generador_glifos import generar_glifo
+from .codificador_linguistico import palabra_a_fractal
+from .kdtree_storage import FractalKDStorage
